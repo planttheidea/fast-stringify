2fcebe0447fe791389e8d6403400bf516e52bb8f
Tony Quetano <tony.quetano@planttheidea.com>
2025-09-25 17:37:49 +0200

    fix release scripts

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 
-import * as React from 'react';
+import * as React from "react";
 
-import stringify from '../src';
-import { isMainThread } from 'worker_threads';
+import stringify from "../src";
+// import { isMainThread } from "worker_threads";
 
 class Foo {
   value: string;
@@ -16,27 +16,30 @@ class Foo {
 const simpleObject = {
   boolean: true,
   fn() {
-    return 'foo';
+    return "foo";
   },
   nan: NaN,
   nil: null,
   number: 123,
-  string: 'foo',
+  string: "foo",
   undef: undefined,
-  [Symbol('key')]: 'value',
+  [Symbol("key")]: "value",
 };
 
 const complexObject = Object.assign({}, simpleObject, {
-  array: ['foo', { bar: 'baz' }],
-  buffer: new Buffer('this is a test buffer'),
-  error: new Error('boom'),
-  foo: new Foo('value'),
-  map: new Map().set('foo', { bar: 'baz' }),
-  object: { foo: { bar: 'baz' } },
-  promise: Promise.resolve('foo'),
+  array: ["foo", { bar: "baz" }],
+  buffer: new Buffer("this is a test buffer"),
+  error: new Error("boom"),
+  foo: new Foo("value"),
+  map: new Map().set("foo", { bar: "baz" }),
+  object: { foo: { bar: "baz" } },
+  promise: Promise.resolve("foo"),
   regexp: /foo/,
-  set: new Set().add('foo').add({ bar: 'baz' }),
-  weakmap: new WeakMap([[{}, 'foo'], [{}, 'bar']]),
+  set: new Set().add("foo").add({ bar: "baz" }),
+  weakmap: new WeakMap([
+    [{}, "foo"],
+    [{}, "bar"],
+  ]),
   weakset: new WeakSet([{}, {}]),
 });
 
@@ -49,25 +52,25 @@ const circularObject = Object.assign({}, complexObject, {
 });
 
 const specialObject = Object.assign({}, complexObject, {
-  react: React.createElement('main', {
+  react: React.createElement("main", {
     children: [
-      React.createElement('h1', { children: 'Title' }),
-      React.createElement('p', { children: 'Content' }),
-      React.createElement('p', { children: 'Content' }),
-      React.createElement('p', { children: 'Content' }),
-      React.createElement('p', { children: 'Content' }),
-      React.createElement('div', {
+      React.createElement("h1", { children: "Title" }),
+      React.createElement("p", { children: "Content" }),
+      React.createElement("p", { children: "Content" }),
+      React.createElement("p", { children: "Content" }),
+      React.createElement("p", { children: "Content" }),
+      React.createElement("div", {
         children: [
-          React.createElement('div', {
-            children: 'Item',
-            style: { flex: '1 1 auto' },
+          React.createElement("div", {
+            children: "Item",
+            style: { flex: "1 1 auto" },
           }),
-          React.createElement('div', {
-            children: 'Item',
-            style: { flex: '1 1 0' },
+          React.createElement("div", {
+            children: "Item",
+            style: { flex: "1 1 0" },
           }),
         ],
-        style: { display: 'flex' },
+        style: { display: "flex" },
       }),
     ],
   }),
@@ -75,54 +78,54 @@ const specialObject = Object.assign({}, complexObject, {
 
 circularObject.deeply.nested.reference = circularObject;
 
-describe('handling of object types', () => {
-  it('should handle simple objects', () => {
+describe("handling of object types", () => {
+  it("should handle simple objects", () => {
     const result = stringify(simpleObject);
 
     expect(result).toEqual(JSON.stringify(simpleObject));
   });
 
-  it('should handle simple objects with a custom replacer', () => {
-    const replacer = (key: string, value: any) =>
-      value && typeof value === 'object' ? value : `primitive-${value}`;
+  it("should handle simple objects with a custom replacer", () => {
+    const replacer = (_key: string, value: any) =>
+      value && typeof value === "object" ? value : `primitive-${value}`;
 
     const result = stringify(simpleObject, replacer);
 
     expect(result).toEqual(JSON.stringify(simpleObject, replacer));
   });
 
-  it('should handle simple objects with indentation', () => {
-    const result = stringify(simpleObject, null, 2);
+  it("should handle simple objects with indentation", () => {
+    const result = stringify(simpleObject, undefined, 2);
 
     expect(result).toEqual(JSON.stringify(simpleObject, null, 2));
   });
 
-  it('should handle complex objects', () => {
+  it("should handle complex objects", () => {
     const result = stringify(complexObject);
 
     expect(result).toEqual(JSON.stringify(complexObject));
   });
 
-  it('should handle complex objects with a custom replacer', () => {
-    const replacer = (key: string, value: any) =>
-      value && typeof value === 'object' ? value : `primitive-${value}`;
+  it("should handle complex objects with a custom replacer", () => {
+    const replacer = (_key: string, value: any) =>
+      value && typeof value === "object" ? value : `primitive-${value}`;
 
     const result = stringify(complexObject, replacer);
 
     expect(result).toEqual(JSON.stringify(complexObject, replacer));
   });
 
-  it('should handle circular objects', () => {
+  it("should handle circular objects", () => {
     const result = stringify(circularObject);
 
     expect(result).toEqual(
       JSON.stringify(
         circularObject,
         (() => {
-          const cache = [];
+          const cache: any[] = [];
 
-          return (key, value) => {
-            if (value && typeof value === 'object' && ~cache.indexOf(value)) {
+          return (_key, value) => {
+            if (value && typeof value === "object" && ~cache.indexOf(value)) {
               return `[ref=.]`;
             }
 
@@ -130,24 +133,24 @@ describe('handling of object types', () => {
 
             return value;
           };
-        })(),
-      ),
+        })()
+      )
     );
   });
 
-  it('should handle circular objects with a custom circular replacer', () => {
+  it("should handle circular objects with a custom circular replacer", () => {
     const result = stringify(
       circularObject,
-      null,
-      null,
-      (key: string, value: string, referenceKey: string) => referenceKey,
+      undefined,
+      undefined,
+      (_key: string, _value: string, referenceKey: string) => referenceKey
     );
     const circularReplacer = (() => {
-      const cache = [];
+      const cache: any[] = [];
 
-      return (key, value) => {
-        if (value && typeof value === 'object' && ~cache.indexOf(value)) {
-          return '.';
+      return (_key: any, value: any) => {
+        if (value && typeof value === "object" && ~cache.indexOf(value)) {
+          return ".";
         }
 
         cache.push(value);
@@ -159,25 +162,25 @@ describe('handling of object types', () => {
     expect(result).toEqual(JSON.stringify(circularObject, circularReplacer));
   });
 
-  it('should handle special objects', () => {
+  it("should handle special objects", () => {
     const result = stringify(specialObject);
 
     expect(result).toEqual(JSON.stringify(specialObject));
   });
 
-  it('should handle special objects with a custom circular replacer', () => {
+  it("should handle special objects with a custom circular replacer", () => {
     const result = stringify(
       specialObject,
-      null,
-      null,
-      (key: string, value: string, referenceKey: string) => referenceKey,
+      undefined,
+      undefined,
+      (_key: string, _value: string, referenceKey: string) => referenceKey
     );
     const circularReplacer = (() => {
-      const cache = [];
+      const cache: any[] = [];
 
-      return (key: string, value: any) => {
-        if (value && typeof value === 'object' && ~cache.indexOf(value)) {
-          return '.';
+      return (_key: string, value: any) => {
+        if (value && typeof value === "object" && ~cache.indexOf(value)) {
+          return ".";
         }
 
         cache.push(value);
@@ -190,10 +193,10 @@ describe('handling of object types', () => {
   });
 });
 
-describe('key references', () => {
-  it('should point to the top level object when it is referenced', () => {
+describe("key references", () => {
+  it("should point to the top level object when it is referenced", () => {
     const object = {
-      foo: 'bar',
+      foo: "bar",
       deeply: {
         recursive: {
           object: {},
@@ -203,12 +206,14 @@ describe('key references', () => {
 
     object.deeply.recursive.object = object;
 
-    expect(stringify(object)).toEqual(`{"foo":"bar","deeply":{"recursive":{"object":"[ref=.]"}}}`);
+    expect(stringify(object)).toEqual(
+      `{"foo":"bar","deeply":{"recursive":{"object":"[ref=.]"}}}`
+    );
   });
 
-  it('should point to the nested object when it is referenced', () => {
+  it("should point to the nested object when it is referenced", () => {
     const object = {
-      foo: 'bar',
+      foo: "bar",
       deeply: {
         recursive: {
           object: {},
@@ -219,7 +224,7 @@ describe('key references', () => {
     object.deeply.recursive.object = object.deeply.recursive;
 
     expect(stringify(object)).toEqual(
-      `{"foo":"bar","deeply":{"recursive":{"object":"[ref=.deeply.recursive]"}}}`,
+      `{"foo":"bar","deeply":{"recursive":{"object":"[ref=.deeply.recursive]"}}}`
     );
   });
 });
